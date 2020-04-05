ce74947847890201a6e374a5e7ccb14933230789
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-05 23:27:46 +0200

    use enums instead of meaningless numbers

---
+++ CommonCodeSmells/MagicNumbers/MeaningfulMagicNumbers.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CommonCodeSmells.MagicNumbers
+{
+    public class MagicNumbers
+    {
+        public void ApproveDocument(DocumentStatus status)
+        {
+            if (status == DocumentStatus.Draft)
+            {
+                // ...
+            }
+            else if (status == DocumentStatus.Lodged)
+            {
+                // ...
+            }
+        }
+
+        public void RejectDoument(DocumentStatus status)
+        {
+            switch (status)
+            {
+                case DocumentStatus.Draft:
+                    // ...
+                    break;
+                case DocumentStatus.Lodged:
+                    // ...
+                    break;
+            }
+        }
+    }
+}
