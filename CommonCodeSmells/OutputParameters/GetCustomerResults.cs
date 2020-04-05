ee6f82deafbd1d3e4355a2b11cc974a26246f735
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-05 20:20:36 +0200

    Moved class to a separate file

---
+++ CommonCodeSmells/OutputParameters/GetCustomerResults.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CleanCode.Comments;
+
+namespace CleanCode.OutputParameters
+{
+    public class GetCustomerResults
+    {
+        public IEnumerable<Customer> Customers { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
