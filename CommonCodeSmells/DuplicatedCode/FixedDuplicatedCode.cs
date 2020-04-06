2bb74a14e0082b499f88602d7ed442a5ec1426c7
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-06 12:01:51 +0200

    Refactor duplicate code efficiently

---
+++ CommonCodeSmells/DuplicatedCode/FixedDuplicatedCode.cs
@@ -0,0 +1,36 @@
+﻿
+namespace CommonCodeSmells.DuplicatedCode
+{
+    class DuplicatedCode
+    {
+        public void AdmitGuest(string name, string admissionDateTime)
+        {
+            // Some logic 
+            // ...
+
+            var tuple = Time.Parse(admissionDateTime);
+
+            // Some more logic 
+            // ...
+            if (tuple.Hours < 10)
+            {
+
+            }
+        }
+
+        public void UpdateAdmission(int admissionId, string name, string admissionDateTime)
+        {
+            // Some logic 
+            // ...
+
+            var tuple = Time.Parse(admissionDateTime);
+
+            // Some more logic 
+            // ...
+            if (tuple.Hours < 10)
+            {
+
+            }
+        }
+    }
+}
---
+++ CommonCodeSmells/DuplicatedCode/Time.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CommonCodeSmells.DuplicatedCode
+{
+    public class Time
+    {
+        public int Minutes;
+        public int Hours;
+
+        public Time(int hours, int minutes)
+        {
+            Hours = hours;
+            Minutes = minutes;
+        }
+
+        public static Time Parse(string param)
+        {
+            if (string.IsNullOrWhiteSpace(param) || !int.TryParse(param.Replace(":", ""), out var time))
+                throw new ArgumentNullException("param");
+
+            var hours = time / 100;
+            var minutes = time % 100;
+
+            return new Time(hours, minutes);
+        }
+    }
+}
